449fc97d287fdfd79723829e2439f351f25ea7fc
AdityaAnuragi <110617126+AdityaAnuragi@users.noreply.github.com>
2024-11-29 14:22:21 +0530

    a basic setup

---
+++ index.ts
@@ -0,0 +1,38 @@
+import express from 'express'
+// import cors from 'cors'
+import { createServer } from 'node:http';
+import { Server } from 'socket.io'
+
+const app = express()
+const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173"
+  }
+})
+
+// app.use(cors())
+
+app.get('/', function (req, res) {
+  // res.send({name: "aditya"})
+  res.json("aditya")
+})
+
+io.on('connection', (socket) => {
+  console.log('Aa frontend has connected to the server');
+
+  socket.on('disconnect', function () {
+    console.log("React app left :(");
+  });
+
+  socket.on('greet', function (msg) {
+    console.log(msg);
+  });
+
+})
+
+server.listen(3000, () => {
+  console.log("Server is live!!")
+})
+
+// console.log("Time to try with the server now!")
